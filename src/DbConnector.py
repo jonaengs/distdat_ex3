ac7669b3c81ba26aa3c69b587deb60d3ec7df735
Jonatan Engstad <jonaengs@gmail.com>
2020-10-12 10:18:22 +0200

    update code with our database credentials

--- src/DbConnector.py
+++ src/DbConnector.py
@@ -1,22 +1,16 @@
 from pymongo import MongoClient, version
 
 
+
+
+
+
 class DbConnector:
-    """
-    Connects to the MongoDB server on the Ubuntu virtual machine.
-    Connector needs HOST, USER and PASSWORD to connect.
-
-    Example:
-    HOST = "tdt4225-00.idi.ntnu.no" // Your server IP address/domain name
-    USER = "testuser" // This is the user you created and added privileges for
-    PASSWORD = "test123" // The password you set for said user
-    """
-
     def __init__(self,
-                 DATABASE='DATABASE_NAME',
-                 HOST="tdt4225-xx.idi.ntnu.no",
-                 USER="TEST_USER",
-                 PASSWORD="test123"):
+                 DATABASE='my_db',
+                 HOST="tdt4225-02.idi.ntnu.no",
+                 USER="read2write",
+                 PASSWORD="kokos"):
         uri = "mongodb://%s:%s@%s/%s" % (USER, PASSWORD, HOST, DATABASE)
         # Connect to the databases
         try:
@@ -25,12 +19,10 @@ class DbConnector:
         except Exception as e:
             print("ERROR: Failed to connect to db:", e)
 
-        # get database information
         print("You are connected to the database:", self.db.name)
         print("-----------------------------------------------\n")
 
     def close_connection(self):
-        # close the cursor
         # close the DB connection
         self.client.close()
         print("\n-----------------------------------------------")
