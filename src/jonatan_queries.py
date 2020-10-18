c82dac8034c0a0c3cc5e5e51aeca1d6ed5cea0ff
Jonatan Engstad <jonaengs@gmail.com>
2020-10-18 15:45:12 +0200

    tiny fix to queries

--- src/jonatan_queries.py
+++ src/jonatan_queries.py
@@ -3,8 +3,7 @@ from itertools import groupby
 import datetime
 
 def query1(db):
-    # plurals_and_names = (('activities', 'Activity'), ('users', 'User'), ('trackpoints', 'TrackPoint'))
-    plurals_and_names = (('persons', 'Person'), )
+    plurals_and_names = (('users', 'User'), ('activities', 'Activity'), ('trackpoints', 'TrackPoint'))
     for c_plural, c_name in plurals_and_names:
         documents = db[c_name].find({}).collection
         print(f"{c_plural} has {documents.count_documents({})} entries")
@@ -21,10 +20,10 @@ def query9(db):
     parse_dt = lambda s: datetime.strptime(s, r"%Y-%m-%d %H:%M:%S")
     all_trackpoints = db["TrackPoints"].find({})
     users = set()
-    for activity_tps in groupby(all_trackpoints, key=lambda tp: tp["activity_id"]):
+    for activity_id, activity_tps in groupby(all_trackpoints, key=lambda tp: tp["activity_id"]):
         for prev_tp, tp in zip(activity_tps[:-1], activity_tps[1:]):
             if parse_dt(tp) - parse_dt(prev_tp) > datetime.timedelta(minutes=5):
-                activity_cursor = db["Activity"].find({"_id": tp["activity_id"]})
+                activity_cursor = db["Activity"].find({"_id": activity_id})
                 user_id = activity_cursor[0]["user_id"]
                 users.add(user_id)
                 break
