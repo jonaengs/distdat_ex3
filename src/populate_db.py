09dc1c85e98319b3a5c229bbf219291efdcdfd0a
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-10-21 21:46:46 +0200

    Fix and rename the populate db script

--- src/populate_db.py
+++ src/populate_db.py
@@ -1,7 +1,7 @@
 import os
 import glob
 from pathlib import Path
-from martin_connector import DbConnector
+from DbConnector import DbConnector
 
 def get_label_path(path):
     splitted_path = path.split('/')
@@ -13,7 +13,7 @@ def get_label_path(path):
     user_path = '/'.join(splitted_path)
     return user_path + '/labels.txt'
 
-def get_label(path, start_date_time, end_date_time):
+def get_label(path, track_point_start_date, track_point_end_date):
     with open(path, 'r') as file:
         lines = file.readlines()
 
@@ -22,10 +22,10 @@ def get_label(path, start_date_time, end_date_time):
             if len(splitted_line) != 5:
                 raise Exception('Label line should have 5 entries')
 
-            start = splitted_line[0] + '+' + splitted_line[1]
-            end = splitted_line[2] + '+' + splitted_line[3]
+            label_start_date = (splitted_line[0] + '+' + splitted_line[1]).replace('/', '-')
+            label_end_date = (splitted_line[2] + '+' + splitted_line[3]).replace('/', '-')
             label = splitted_line[4]
-            if start == start_date_time and end == end_date_time:
+            if label_start_date == track_point_start_date and label_end_date == track_point_end_date:
                 return label
         
         return False
@@ -39,7 +39,7 @@ def get_track_point(track_point_line, id, activity_id):
     splitted_track_point = track_point_line.split(',')
     if len(splitted_track_point) != 7:
         raise Exception('Track point line should have 7 entries')
-    
+
     track_point = {
         '_id': id,
         'lat': float(splitted_track_point[0]),
@@ -55,7 +55,7 @@ def main():
     activity_id_counter = 1
     track_point_id_counter = 1
     db = DbConnector().db
-    path = Path(__file__).parent.absolute() # path = Path(__file__).parent.parent.absolute()
+    path = Path(__file__).parent.parent.absolute()
 
     for filepath in glob.iglob(r'%s/**/*.plt' % path, recursive=True):
         with open(filepath, 'r') as file:
@@ -88,12 +88,13 @@ def main():
                 track_point = get_track_point(track_point_line, track_point_id_counter, activity_id_counter)
                 track_points.append(track_point)
                 track_point_id_counter += 1
-            
+
             activity_id_counter += 1
 
+                
             db['Activity'].insert_one(activity)
             db['TrackPoint'].insert_many(track_points)
-            print('FERDIG MED FIL!\n')
+            print('FERDIG MED FIL %s\n' % activity_id_counter)
 
 def drop():
     db = DbConnector().db
