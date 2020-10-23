11d330f76174e300f3d0f87d48b03d07bdaedc07
Jonatan Engstad <jonaengs@gmail.com>
2020-10-23 13:20:16 +0200

    fix query 9

--- src/jonatan_queries.py
+++ src/jonatan_queries.py
@@ -1,26 +1,26 @@
-from collections import Counter
-from itertools import groupby
-from datetime import datetime, timedelta
+from collections import Counter, defaultdict
+from datetime import timedelta
+from pprint import pprint
 
 from DbConnector import DbConnector
 
 
-def query1(db):
-    plurals_and_names = (('users', 'User'), ('activities', 'Activity'), ('trackpoints', 'TrackPoint'))
-    for c_plural, c_name in plurals_and_names:
-        documents = db[c_name].find({}).collection
-        print(f"{c_plural} has {documents.count_documents({})} entries")
+def query1():
+    for c_name in ("User", "Activity", "TrackPoint"):
+        num_docs = db[c_name].count_documents({})
+        print(f"{c_name} has {num_docs} entries")
 
 
-def query5(db):
+def query5():
     activities_with_transport = db["Activity"].find({
         "transportation_mode": {"$exists": True, "$ne": None}
-    })
+    }, {"transportation_mode": 1})
     transports = (a["transportation_mode"] for a in activities_with_transport)
+    print("Count of registered transportation modes:")
     print(Counter(transports))
 
 
-def query9(db):  # antar trackpoint peker til aktivitet som peker til bruker
+def query9():
     activity_user_map = {
         doc["_id"]: doc["user_id"] for doc in
         db.Activity.find({}, {"_id": 1, "user_id": 1})
@@ -30,23 +30,19 @@ def query9(db):  # antar trackpoint peker til aktivitet som peker til bruker
             "datetimes": {"$push": "$date_time"}}
         }], allowDiskUse=True)
     
-    users = set()
+    num_invalid = defaultdict(int)
     for doc in grouped_trackpoints:
         aid, tp_dts = doc["_id"], doc["datetimes"]
-        if activity_user_map[aid] in users:  # skip 
-            continue
         for prev_dt, dt in zip(tp_dts[:-1], tp_dts[1:]):
             if (dt - prev_dt > timedelta(minutes=5)):
-                users.add(activity_user_map[aid])
-                break
-
-    print(*sorted(users), sep=", ")
-
+                num_invalid[activity_user_map[aid]] += 1
+    print("Number of invalid activities per user:")
+    print(dict(num_invalid))
 
 
 if __name__ == '__main__':
     db = DbConnector().db
-    query1(db)
-    query5(db)
-    query9(db)
+    query1()
+    query5()
+    query9()
 
